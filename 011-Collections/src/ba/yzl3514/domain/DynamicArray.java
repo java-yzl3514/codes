3747469a9804c3796643d5d8d619dff6b78087df
Önder Şahin <developer.ondersahin@gmail.com>
2025-10-19 20:12:41 +0300

    Getting started with collections

---
+++ 011-Collections/src/ba/yzl3514/domain/DynamicArray.java
@@ -0,0 +1,86 @@
+package ba.yzl3514.domain;
+
+import java.util.Arrays;
+
+/**
+ *
+ *
+ *
+ * @author Onder Sahin
+ *
+ */
+public class DynamicArray {
+    private static final int DEFAULT_CAPACITY = 1;
+    private Object[] data;
+    private int size;
+
+    public DynamicArray() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public DynamicArray(int capacity) {
+        if (capacity < DEFAULT_CAPACITY)
+            capacity = DEFAULT_CAPACITY;
+        data = new Object[capacity];
+    }
+
+    public void add(Object item){
+        if(size == data.length){
+            grow(size + 1);
+        }
+        data[size++] = item;
+    }
+
+    private Object[] grow(int minCapacity){
+        int oldCapacity = data.length;
+        int newCapacity = calculateNeedLength(oldCapacity, minCapacity - oldCapacity, oldCapacity >> 1 ); // x1.5 Altın Oran
+        return data = Arrays.copyOf(data,newCapacity);
+    }
+
+    private int calculateNeedLength(int oldCapacity, int minCapacity, int prefCapacity){
+        int newCapacity = oldCapacity + Math.max(minCapacity, prefCapacity);
+        if(newCapacity > 0 && newCapacity <= Integer.MAX_VALUE - 8) // JVM Object Header implementation
+            return newCapacity;
+        else {
+            // huge !!
+            newCapacity = oldCapacity + minCapacity;
+            if(newCapacity < 0) {
+                // TODO : throw exception
+            }else if(newCapacity < Integer.MAX_VALUE - 8){ // JVM Object Header implementation
+                return Integer.MAX_VALUE - 8; // JVM Object Header implementation
+            }
+            return newCapacity;
+        }
+    }
+
+
+    public Object get(int index){
+        if(index < 0 || index >= size){
+            // TODO throw illegal index
+            return null;
+        }
+        return data[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    public boolean contains(Object item){
+        // TODO :
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("[");
+        for(int i = 0; i < size; i++){
+            builder.append(data[i]);
+            if(i < size - 1)
+                builder.append(", ");
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+}
---
+++ 011-Collections/src/ba/yzl3514/main/Driver.java
@@ -0,0 +1,34 @@
+package ba.yzl3514.main;
+
+import ba.yzl3514.domain.DynamicArray;
+
+import java.util.ArrayList;
+
+/**
+ *
+ *
+ *
+ * @author Onder Sahin
+ *
+ */
+public class Driver {
+
+    public static void main(String[] args) {
+
+        DynamicArray cart = new DynamicArray(3);
+        cart.add("Water");
+        cart.add("Apple");
+        cart.add("Milk");
+        cart.add("Soap");
+
+        System.out.println(cart);
+
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Water");
+        list.add("Apple");
+        list.add("Milk");
+        list.add("Soap");
+        System.out.println(list);
+    }
+}
